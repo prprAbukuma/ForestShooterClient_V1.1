29860efc119a7bfb602a2dc754cf776abc38fc4b
prprAbukuma <tanyu159@live.com>
2019-03-15 16:34:28 +0800

    fixed all bugs and add a function HP silder

---
+++ ForestWarOnline/Assets/Scripts/Request/SyncHPRequest.cs
@@ -0,0 +1,29 @@
+﻿using Common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncHPRequest : BaseRequest {
+    private GamePanel _gamePanel;
+    public override void Awake()
+    {
+        requestCode = RequestCode.Game;
+        actionCode = ActionCode.SyncHP;
+        base.Awake();
+    }
+    private void Start()
+    {
+        _gamePanel = this.GetComponent<GamePanel>();
+    }
+
+    public override void OnResponse(string data)
+    {
+        //解析血量信息
+        string[] HPdataStr = data.Split('#');
+        int p1currentHP = int.Parse(HPdataStr[0]);
+        int p2currentHP = int.Parse(HPdataStr[1]);
+        //交给gamepanel处理
+        _gamePanel.HandleSyncHPSlider(p1currentHP,p2currentHP);
+    }
+
+}
